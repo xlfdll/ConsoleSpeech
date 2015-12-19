1897070e406d513a6696ea63e7aec3b3acee8140
xlfdll <xlfdll@gmail.com>
2015-12-19 22:17:54 +0900

    Version 1.0: Initial Release

---
+++ ConsoleSpeech/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Speech.Synthesis;
+
+namespace ConsoleSpeech
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Console Speech");
+            Console.WriteLine("(C) 2009 Xlfdll Workstation");
+
+            Console.WriteLine();
+
+            synth.Volume = 100;
+            synth.SelectVoiceByHints(VoiceGender.NotSet, VoiceAge.NotSet, 0, CultureInfo.CurrentCulture);
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter below to speak:");
+                Console.WriteLine();
+
+                while (true)
+                {
+                    Console.Write("> ");
+
+                    synth.Speak(Console.ReadLine());
+                }
+            }
+            else
+            {
+                foreach (String item in args)
+                {
+                    synth.Speak(item);
+                }
+            }
+        }
+
+        static SpeechSynthesizer synth = new SpeechSynthesizer();
+    }
+}
---
+++ ConsoleSpeech/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Console Speech")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Xlfdll Workstation")]
+[assembly: AssemblyProduct("Console Speech")]
+[assembly: AssemblyCopyright("© 2009 Xlfdll Workstation")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c06f7f94-a53e-4825-967a-3eb7aa083b26")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
